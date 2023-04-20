270394fb730630c59251d10d5fb2c1e41ec45654
Dhivya S <dhivyasubashchandran@gmail.com>
2023-04-20 23:05:47 +0530

    Update MiddleOfTheLinkedList.c

--- MiddleOfTheLinkedList.c
+++ MiddleOfTheLinkedList.c
@@ -1,3 +1,4 @@
+//approach 1 - using count
 /**
  * Definition for singly-linked list.
  * struct ListNode {
@@ -23,4 +24,42 @@ struct ListNode* middleNode(struct ListNode* head){
     }
     printf("%d",head->val);
     return head;
+}
+// approach 2 - using 2 pointers
+
+/*
+    we have 2 pointer called middle and end both pointing to the first element. 
+    The logic behind this is for every 2 elements in the list the middle value moves by 1.
+    
+    let's take an example 1 2 3 4 5 6 7 8
+    at first m and e are at 1
+    then 1 2 3 4 5 6 7 8    
+           m e
+         1 2 3 4 5 6 7 8
+             m e
+         1 2 3 4 5 6 7 8
+             m   e
+         1 2 3 4 5 6 7 8
+               m   e
+         1 2 3 4 5 6 7 8
+               m     e
+         1 2 3 4 5 6 7 8
+                 m     e
+*/                   
+
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     struct ListNode *next;
+ * };
+ */
+struct ListNode* middleNode(struct ListNode* head){
+    struct ListNode *middle=head;
+    struct ListNode *end=head;
+    while(end!=NULL && end->next!=NULL){
+        end=end->next->next;
+        middle=middle->next;
+    } 
+    return middle;
 }
